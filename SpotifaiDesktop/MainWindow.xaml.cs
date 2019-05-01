50a0782b38ae5b248d83b5056087503c809bb29e
Eagle Cheow <eagle.cheow@hotmail.com>
2019-05-01 11:09:12 +0800

    Added more platforms to be supported in the future

---
+++ SpotifaiDesktop/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using SpotifaiStandard.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SpotifaiDesktop
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            TestScript();
+        }
+
+        public void TestScript()
+        {
+            Track track = new Track()
+            {
+                Album = new Album(),
+                Artists = new Artist[] { new Artist() },
+                AvailableMarkets = new string[] { "en", "us" },
+                DiscNumber = 3,
+                DurationMs = 3000,
+                Explicit = false,
+                Id = "A0021A",
+                IsPlayable = true,
+                Name = "LATATA",
+                PreviewUrl = "www.google.com",
+                TrackNumber = 2,
+                Uri = "hhhahaha:ssddf"
+            };
+
+            Debug.WriteLine("Current track: " + track.Name);
+        }
+    }
+}
---
+++ SpotifaiMobile/SpotifaiMobile/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using SpotifaiStandard.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace SpotifaiMobile
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(true)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void TestScript()
+        {
+            Track track = new Track()
+            {
+                Album = new Album(),
+                Artists = new Artist[] { new Artist() },
+                AvailableMarkets = new string[] { "en", "us" },
+                DiscNumber = 3,
+                DurationMs = 3000,
+                Explicit = false,
+                Id = "A0021A",
+                IsPlayable = true,
+                Name = "LATATA",
+                PreviewUrl = "www.google.com",
+                TrackNumber = 2,
+                Uri = "hhhahaha:ssddf"
+            };
+
+            Debug.WriteLine("Current track: " + track.Name);
+        }
+
+        private void TestButton_Clicked(object sender, EventArgs e)
+        {
+            TestScript();
+        }
+    }
+}
